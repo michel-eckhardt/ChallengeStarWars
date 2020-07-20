8497d41d4e7bd25a94516e5970afbf5aeabebfce
Michel Eckhardt <michel.desenvolvedor@gmail.com>
2020-07-20 18:55:43 -0300

    added dependencies

---
+++ src/main/java/br/com/b2w/starWars/StarWarsApplication.java
@@ -0,0 +1,13 @@
+package br.com.b2w.starWars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarWarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarWarsApplication.class, args);
+	}
+
+}
